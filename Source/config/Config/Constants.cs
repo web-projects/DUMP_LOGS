2dfb72059da360fa76288a42b0a41f9b9d01d414
Jon <jon.bianco@trustcommerce.com>
2022-09-30 18:18:20 -0700

    Initial commit

---
+++ Source/config/Config/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.Config.Config
+{
+    public static class Constants
+    {
+        public const string TargetDummyFile = "dummy.log";
+        public const string TargetDirectory = @"C:\Users\Public\Roaming\Sphere";
+    }
+}
---
+++ Source/devices/Core/State/SubWorkflows/Actions/DeviceGetTerminalLogsSubStateAction.cs
@@ -0,0 +1,68 @@
+﻿using Common.XO.Device;
+using Common.XO.Requests;
+using Devices.Common.Interfaces;
+using Devices.Core.Cancellation;
+using Devices.Core.Helpers;
+using Devices.Core.State.Enums;
+using System;
+using System.Threading.Tasks;
+using static Devices.Core.State.Enums.DeviceSubWorkflowState;
+
+namespace Devices.Core.State.SubWorkflows.Actions
+{
+    internal class DeviceGetTerminalLogsSubStateAction : DeviceBaseSubStateAction
+    {
+        public override DeviceSubWorkflowState WorkflowStateType => GetTerminalLogs;
+
+        public DeviceGetTerminalLogsSubStateAction(IDeviceSubStateController _) : base(_) { }
+
+        public override SubStateActionLaunchRules LaunchRules => new SubStateActionLaunchRules
+        {
+            RequestCancellationToken = true
+        };
+
+        public override async Task DoWork()
+        {
+            if (StateObject is null)
+            {
+                //_ = Controller.LoggingClient.LogErrorAsync("Unable to find a state object while attempting to obtain terminal logs.");
+                Console.WriteLine("Unable to find a state object while attempting to obtain terminal logs.");
+                _ = Error(this);
+            }
+            else
+            {
+                LinkRequest linkRequest = StateObject as LinkRequest;
+
+                foreach (LinkActionRequest linkActionRequest in linkRequest.Actions)
+                {
+                    LinkDeviceIdentifier deviceIdentifier = linkActionRequest.DALRequest.DeviceIdentifier;
+                    IDeviceCancellationBroker cancellationBroker = Controller.GetDeviceCancellationBroker();
+
+                    ICardDevice cardDevice = FindTargetDevice(deviceIdentifier);
+                    if (cardDevice != null)
+                    {
+                        var timeoutPolicy = await cancellationBroker.ExecuteWithTimeoutAsync<LinkActionRequest>(
+                            _ => cardDevice.GetTerminalLogs(linkActionRequest),
+                            DeviceConstants.TerminalDumpLogsTimeout,
+                            this.CancellationToken);
+
+                        if (timeoutPolicy.Outcome == Polly.OutcomeType.Failure)
+                        {
+                            //_ = Controller.LoggingClient.LogErrorAsync($"Unable to process VIPA DUMP LOGS request from device - '{Controller.DeviceEvent}'.");
+                            Console.WriteLine($"Unable to process VIPA DUMP LOGS request from device - '{Controller.DeviceEvent}'.");
+                            BuildSubworkflowErrorResponse(linkRequest, cardDevice.DeviceInformation, Controller.DeviceEvent);
+                        }
+                    }
+                    else
+                    {
+                        UpdateRequestDeviceNotFound(linkRequest, deviceIdentifier);
+                    }
+                }
+
+                Controller.SaveState(linkRequest);
+
+                _ = Complete(this);
+            }
+        }
+    }
+}
