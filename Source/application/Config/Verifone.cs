68fd2145e7133182aa55b56e1bb040e22b85edcb
Jon <jon.bianco@trustcommerce.com>
2022-10-01 20:51:07 -0700

    Restored original config

--- Source/application/Config/Verifone.cs
+++ Source/application/Config/Verifone.cs
@@ -2,6 +2,8 @@
 {
     public class Verifone
     {
+        public int SortOrder { get; set; }
+        public string[] SupportedDevices { get; set; }
         public string Comport { get; set; }
         public string[] ADKLoggerBundles { get; set; }
         public bool EnableADKLogger { get; set; }
